ed76b183b8f8d638ccc15bf058a06481b9ecbf89
jnrubianoqantica <nicolas.rubiano@q-antica.com>
2016-09-16 21:52:31 -0500

    V1-Usuario

---
+++ src/com/qantica/fedemarket/managebean/UsuarioManage.java
@@ -0,0 +1,81 @@
+﻿package com.qantica.fedemarket.managebean;
+
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+
+import com.qantica.fedemarket.ejb.UsuarioBeanLocal;
+import com.qantica.fedemarket.entidad.Usuario;
+
+@ManagedBean
+public class UsuarioManage {
+
+	@EJB(name = "UsuarioBean/local")
+	UsuarioBeanLocal miEJB;
+
+	Usuario usuario;
+	int id;
+
+	String nombreUsuario;
+	String contrasena;
+
+	/**
+	 * METODOS ADICION, BUSQUEDA Y ACTUALIZACION
+	 */
+
+	public void adicionarUsuario() {
+		miEJB.adicionarUsuario(usuario);
+		usuario = new Usuario();
+	}
+
+	public void buscar() {
+		miEJB.buscarUsuario(id);
+	}
+
+	public void actualizar() {
+		miEJB.actualizarUsuario(usuario);
+	}
+
+	public void login() {
+		
+
+	}
+
+	/**
+	 * METODOS ACCESORES Y MODIFICADORES
+	 * 
+	 * @return
+	 */
+
+	public Usuario getUsuario() {
+		return usuario;
+	}
+
+	public void setUsuario(Usuario usuario) {
+		this.usuario = usuario;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNombreUsuario() {
+		return nombreUsuario;
+	}
+
+	public void setNombreUsuario(String nombreUsuario) {
+		this.nombreUsuario = nombreUsuario;
+	}
+
+	public String getContrasena() {
+		return contrasena;
+	}
+
+	public void setContrasena(String contrasena) {
+		this.contrasena = contrasena;
+	}
+
+}
---
+++ src/com/qantica/fedemarket/servlet/ServletLogin.java
@@ -0,0 +1,112 @@
+package com.qantica.fedemarket.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.ejb.EJB;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NameNotFoundException;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.qantica.fedemarket.ejb.UsuarioBeanRemote;
+import com.qantica.fedemarket.entidad.Usuario;
+
+
+public class ServletLogin extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	Context context;
+
+	@EJB(name="UsuarioBean/remote")
+	UsuarioBeanRemote miEJB;
+
+	public void init() {
+		try {			
+			context = new InitialContext();
+			miEJB = (UsuarioBeanRemote) context.lookup("UsuarioBean/remote");	
+		} catch (NameNotFoundException e) {			
+		}catch (Exception e) {
+			System.out.println("Excepción");
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ServletLogin() {
+		super();
+
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		try {
+			//Defino los parametros que capturo por GET
+			String nombre = request.getParameter("nombre_usuario");
+			String contrasena = request.getParameter("contrasena");
+			
+			//Declaro un Usuario que contendrá la respuesta del bean 
+			Usuario aux = miEJB.login(nombre, contrasena);
+			System.out.println("USUARIO -----> "+aux);
+			
+			//Defino la decodificación de la respuesta		
+			response.setHeader("Content-Type", "text/html; charset=UTF-8");
+			response.setContentType("text/html;charset=UTF-8");
+			response.setCharacterEncoding("UTF-8");
+			
+			PrintWriter out = response.getWriter();
+			if(aux != null){
+				out.println(aux.toString());
+			}else{
+				out.println("false");
+			}
+			
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException, NullPointerException {
+		try {
+			//Defino los parametros que capturo por POST
+			String nombre = request.getParameter("nombre_usuario");
+			String contrasena = request.getParameter("contrasena");
+			
+			//Declaro un Usuario que contendrá la respuesta del bean 
+			Usuario aux = miEJB.login(nombre, contrasena);
+			System.out.println("USUARIO -----> "+aux);
+			
+			//Defino la decodificación de la respuesta
+			response.setHeader("Content-Type", "text/html; charset=UTF-8"); 
+			response.setContentType("text/html;charset=UTF-8");
+			response.setCharacterEncoding("UTF-8");			
+			
+			PrintWriter out = response.getWriter();
+			
+			if(aux != null){
+				out.println(aux.toString());
+			}else{
+				out.println("false");
+			}			
+			out.close();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
