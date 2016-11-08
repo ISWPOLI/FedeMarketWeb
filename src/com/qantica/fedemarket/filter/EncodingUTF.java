96eaa9693d66837e3c0ef5879ac1881afc5b1940
jnrubianoqantica <jrubiaob@usersad.everis.int>
2016-11-07 21:43:51 -0500

    ModificacionContext

---
+++ src/com/qantica/fedemarket/filter/EncodingUTF.java
@@ -0,0 +1,43 @@
+package com.qantica.fedemarket.filter;
+
+import java.io.IOException;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * Servlet Filter implementation class EncodingUTF
+ */
+public class EncodingUTF implements Filter {
+
+	public EncodingUTF() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public void destroy() {
+		// TODO Auto-generated method stub
+	}
+
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException {
+
+		
+		if (request.getCharacterEncoding() == null) {
+
+			if (request.getContentType()!=null && request.getContentType().contains("multipart/form-data")) {
+				request.setCharacterEncoding("ISO-8859-1");
+			} else {
+				request.setCharacterEncoding("UTF-8");
+			}
+		}
+		chain.doFilter(request, response);
+	}
+
+	public void init(FilterConfig fConfig) throws ServletException {
+
+	}
+
+}
